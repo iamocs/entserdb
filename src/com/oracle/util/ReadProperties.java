9baaa716483fb23c790209d5968db410fb267e43
ruben <ruben@dhcp-london-wlan-10-164-67-195.uk.oracle.com>
2014-09-11 13:36:58 +0200

    minor bug corrections

---
+++ src/com/oracle/util/ReadProperties.java
@@ -0,0 +1,38 @@
+package com.oracle.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class ReadProperties {
+    Properties prop;
+    
+    public static void main (String[] args){
+        
+    }
+    
+    public Properties ReadProperties() throws IOException {
+        return ReadProperties(null);
+    }
+    
+    public Properties ReadProperties (String propertiesFileName) throws IOException {
+        if (propertiesFileName==null){
+            propertiesFileName="config.properties";
+    }
+        prop = new Properties();
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propertiesFileName);
+        prop.load(inputStream);
+        
+        if (inputStream == null) {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
+        
+        return prop;
+    }
+        
+}
